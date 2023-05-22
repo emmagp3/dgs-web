58579dd26fc89fb8b09c99f1df77b0d4eb3cd323
Emmanuel Valentin <ema_110@outlook.es>
2023-05-21 18:11:20 -0600

    Se añadió la página productos

---
+++ src/pages/productos/index.tsx
@@ -0,0 +1,28 @@
+import Layout from '@/components/layout/layout';
+import { GetStaticProps } from 'next';
+import { getAllProductsCard } from '../../../lib/stores';
+import type { ProductCardProps } from '@/components/product_card/product_card';
+import ProductCard from '@/components/product_card/product_card';
+
+export default function Product({
+  products,
+}: {
+  products: ProductCardProps[];
+}) {
+  return (
+    <Layout title="Productos">
+      {products.map((product) => (
+        <ProductCard key={product.model} {...product} />
+      ))}
+    </Layout>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const products = getAllProductsCard();
+  return {
+    props: {
+      products,
+    },
+  };
+};
