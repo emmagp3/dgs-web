65f2c116d2b5810563ee655f7ec5408aeb9474e3
Emmanuel Valentin <ema_110@outlook.es>
2023-06-06 10:38:54 -0600

    Se agregó el componente footer.

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,22 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div>
+        <p>Ubicación</p>
+        <p>Calle S/N</p>
+        <p>Delegación</p>
+        <p>Cuidad de México</p>
+        <p>CP: XXXXXX</p>
+      </div>
+      <div>
+        <p>Horarios</p>
+        <p>Lun-Vie: xx:xx - xx:xx</p>
+        <p>Sábados: xx:xx - xx:xx</p>
+        <p>Domingos: xx:xx - xx:xx</p>
+      </div>
+      <div>
+        <p>¡Síguenos en nuestras redes sociales!</p>
+      </div>
+    </footer>
+  );
+}
