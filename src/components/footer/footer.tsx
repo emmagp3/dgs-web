dcb9ad483be9bafd1aa0e2341e5c606572a7e3d6
Emmanuel Valentin <ema_110@outlook.es>
2023-07-10 17:54:51 -0600

    Se agregaron estilos para el footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,18 +1,22 @@
+import styles from './footer.module.css';
+
 export default function Footer() {
   return (
-    <footer>
-      <div>
-        <p>Ubicación</p>
-        <p>Calle S/N</p>
-        <p>Delegación</p>
-        <p>Cuidad de México</p>
-        <p>CP: XXXXXX</p>
-      </div>
-      <div>
-        <p>Horarios</p>
-        <p>Lun-Vie: xx:xx - xx:xx</p>
-        <p>Sábados: xx:xx - xx:xx</p>
-        <p>Domingos: xx:xx - xx:xx</p>
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <div>
+          <h2>Ubicación</h2>
+          <p>Calle S/N</p>
+          <p>Delegación</p>
+          <p>Cuidad de México</p>
+          <p>CP: XXXXXX</p>
+        </div>
+        <div>
+          <h2>Horarios</h2>
+          <p>Lun-Vie: xx:xx - xx:xx</p>
+          <p>Sábados: xx:xx - xx:xx</p>
+          <p>Domingos: xx:xx - xx:xx</p>
+        </div>
       </div>
       <div>
         <p>¡Síguenos en nuestras redes sociales!</p>
