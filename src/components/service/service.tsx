64c841c36958c70a892802bbb2a97e7ccc4e5be4
Emmanuel Valentin <ema_110@outlook.es>
2023-07-10 17:56:57 -0600

    Se añadieron estilos para el componente service

--- src/components/service/service.tsx
+++ src/components/service/service.tsx
@@ -1,11 +1,13 @@
 import { ServiceModel } from '../../../lib/models';
+import Button from '../button/button';
+import styles from './service.module.css';
 
 export default function Service({ name, description }: ServiceModel) {
   return (
-    <article>
-      <h2>{name}</h2>
-      <p>{description}</p>
-      <button>Más información</button>
-    </article>
+    <div className={`${styles.service} flex flex-column`}>
+      <h2 className={styles.title}>{name}</h2>
+      <p className={styles.description}>{description}</p>
+      <Button variant="primary">Más información</Button>
+    </div>
   );
 }
