03ced248b2891873615f309f2b22d09f2e9d72cb
Emmanuel Valentin <ema_110@outlook.es>
2023-07-20 23:40:46 -0600

    Se añadieron más productos de prueba

--- lib/stores/product.store.ts
+++ lib/stores/product.store.ts
@@ -42,6 +42,20 @@ const initStore = () => {
         'Ut voluptate labore sint id esse aliqua. Cupidatat reprehenderit dolor esse voluptate commodo non cillum aliqua labore. Excepteur magna reprehenderit ut eu et do. Enim reprehenderit aliquip reprehenderit culpa adipisicing aliqua et Lorem incididunt. Sunt commodo consectetur proident qui. Magna pariatur ex nostrud commodo quis nisi deserunt reprehenderit Lorem. In occaecat nostrud aute qui laboris elit ut officia reprehenderit quis dolore nulla eiusmod laborum. Eu id dolore incididunt nostrud consequat. Aute ex est est labore elit ullamco tempor esse magna excepteur. Fugiat occaecat velit labore nulla id quis nisi. Cillum incididunt eu anim eu non aliqua dolor sunt anim minim consectetur incididunt. Qui cupidatat dolore ea aute commodo sint dolore eu dolore eiusmod. Incididunt nostrud sit nulla consequat ex eiusmod aliquip. Lorem laborum proident ad adipisicing consequat mollit enim culpa ullamco.',
       image: 'https://via.placeholder.com/200',
     },
+    {
+      model: 'producto-5',
+      name: 'Producto 5',
+      description:
+        'Ut voluptate labore sint id esse aliqua. Cupidatat reprehenderit dolor esse voluptate commodo non cillum aliqua labore. Excepteur magna reprehenderit ut eu et do. Enim reprehenderit aliquip reprehenderit culpa adipisicing aliqua et Lorem incididunt. Sunt commodo consectetur proident qui. Magna pariatur ex nostrud commodo quis nisi deserunt reprehenderit Lorem. In occaecat nostrud aute qui laboris elit ut officia reprehenderit quis dolore nulla eiusmod laborum. Eu id dolore incididunt nostrud consequat. Aute ex est est labore elit ullamco tempor esse magna excepteur. Fugiat occaecat velit labore nulla id quis nisi. Cillum incididunt eu anim eu non aliqua dolor sunt anim minim consectetur incididunt. Qui cupidatat dolore ea aute commodo sint dolore eu dolore eiusmod. Incididunt nostrud sit nulla consequat ex eiusmod aliquip. Lorem laborum proident ad adipisicing consequat mollit enim culpa ullamco.',
+      image: 'https://via.placeholder.com/200',
+    },
+    {
+      model: 'producto-6',
+      name: 'Producto 6',
+      description:
+        'Ut voluptate labore sint id esse aliqua. Cupidatat reprehenderit dolor esse voluptate commodo non cillum aliqua labore. Excepteur magna reprehenderit ut eu et do. Enim reprehenderit aliquip reprehenderit culpa adipisicing aliqua et Lorem incididunt. Sunt commodo consectetur proident qui. Magna pariatur ex nostrud commodo quis nisi deserunt reprehenderit Lorem. In occaecat nostrud aute qui laboris elit ut officia reprehenderit quis dolore nulla eiusmod laborum. Eu id dolore incididunt nostrud consequat. Aute ex est est labore elit ullamco tempor esse magna excepteur. Fugiat occaecat velit labore nulla id quis nisi. Cillum incididunt eu anim eu non aliqua dolor sunt anim minim consectetur incididunt. Qui cupidatat dolore ea aute commodo sint dolore eu dolore eiusmod. Incididunt nostrud sit nulla consequat ex eiusmod aliquip. Lorem laborum proident ad adipisicing consequat mollit enim culpa ullamco.',
+      image: 'https://via.placeholder.com/200',
+    },
   ];
   state.products = products;
 };
