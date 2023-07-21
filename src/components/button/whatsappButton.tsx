a8ecff2d80aa4193b525fe6038bb8f4b15b937f0
Emmanuel Valentin <ema_110@outlook.es>
2023-07-20 23:47:05 -0600

    Se cambió el botón por el componente Button en el caso de 'Contactar el vendedor'

--- src/components/button/whatsappButton.tsx
+++ src/components/button/whatsappButton.tsx
@@ -1,21 +1,19 @@
 import React, { MouseEventHandler } from 'react';
+import Button from './button';
 
 interface ContactButtonProps {
   image?: string;
   onClick: MouseEventHandler;
 }
 
-export default function ContactButton({
-  image = undefined,
-  onClick,
-}: ContactButtonProps) {
-  return (
+export default function ContactButton({ image, onClick }: ContactButtonProps) {
+  return image ? (
     <button onClick={onClick}>
-      {image ? (
-        <img src={image} alt="Whatsapp Icon" />
-      ) : (
-        'Contactar al vendedor'
-      )}
+      <img src="whatsapp.svg" alt="Whatsapp Logo" />
     </button>
+  ) : (
+    <Button variant="primary" onClick={onClick}>
+      Contactar al vendedor
+    </Button>
   );
 }
